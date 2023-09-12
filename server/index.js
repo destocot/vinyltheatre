b649796aa127afa025adb598cd359d2ec24d4c9f
destocot <khurramcali@gmail.com>
2023-09-12 19:15:55 -0400

    origin

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://vinyltheatre-mvp.vercel.app/"
   methods: ['POST', 'PUT', 'GET', 'DELETE', 'HEAD'],
   credentials: true
 }))
