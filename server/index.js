07275d03a815f1bf7b1472968ae2ac9a3e793286
destocot <khurramcali@gmail.com>
2023-09-12 20:56:16 -0400

    redo

--- server/index.js
+++ server/index.js
@@ -9,30 +9,29 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors({
-//   origin: ["https://vinyltheatre-mvp.vercel.app"],
-//   methods: ['POST', 'PUT', 'GET', 'DELETE', 'HEAD'],
-//   credentials: true
-// }))
-app.use(cors());
+app.use(cors({
+  origin: ["https://vinyltheatre-mvp.vercel.app"],
+  methods: ['POST', 'PUT', 'GET', 'DELETE', 'HEAD'],
+  credentials: true
+}))
 
 
 const mainRouter = require('./routes/mainRoutes');
 app.use('/', mainRouter);
 
-// const discogsRouter = require('./routes/discogsRoutes');
-// app.use('/api', discogsRouter);
+const discogsRouter = require('./routes/discogsRoutes');
+app.use('/api', discogsRouter);
 
-// const dashboardRouter = require('./routes/dashboardRoutes');
-// app.use('/dashboard', dashboardRouter);
+const dashboardRouter = require('./routes/dashboardRoutes');
+app.use('/dashboard', dashboardRouter);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`.yellow))
 
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log("Connected to MongoDB".magenta)
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB".magenta)
+    app.listen(port, () => console.log(`Listening at http://localhost:${port}`.yellow))
+  })
+  .catch((error) => {
+    console.log(error);
+  })
