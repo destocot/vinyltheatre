e56d4998cc0ae481bb8353cedf01da102f10d202
destocot <khurramcali@gmail.com>
2023-09-12 22:16:34 -0400

    new grounds 1

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Next we make an 'instance' of it
 const instance = axios.create({
   // .. where we make our configurations
-  // baseURL: 'http://localhost:5001/'
+  // baseURL: 'http://localhost:5001'
   baseURL: 'https://vinyltheatre-mvp-srvr1.onrender.com'
 });
 
